f4f41915b3df956b1eb36beb7360dc8ed1ff6d10
Victor Govedice <69945592+VictorGovedice@users.noreply.github.com>
2025-03-19 13:05:39 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './app/AppNavigator';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
